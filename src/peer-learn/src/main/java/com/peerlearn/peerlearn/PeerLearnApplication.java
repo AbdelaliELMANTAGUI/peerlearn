7e2b20488b70a1733ccf7719a3853316fee4fd67
abdelali elmantagui <abdelalielmantagui@gmail.com>
2022-05-29 16:00:57 +0100

    :tada: create a project for api

---
+++ src/peer-learn/src/main/java/com/peerlearn/peerlearn/PeerLearnApplication.java
@@ -0,0 +1,13 @@
+package com.peerlearn.peerlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerLearnApplication.class, args);
+    }
+
+}
